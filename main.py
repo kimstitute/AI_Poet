603bd8bbbe3308c58978722415dfc33e6437a343
kimstitute <152252175+kimstitute@users.noreply.github.com>
2024-10-14 20:51:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ chat_model = ChatOpenAI()
 # result = chat_model.invoke(f"{subject}에 대한 시를 써줘")
 # print(result.content)
 
-st.title("인공지능 시인")
+st.title("인공지능 시인 Created by Minsang")
 subject = st.text_input("시의 주제를 입력해주세요.")
 st.write(f"시의 주제: {subject}")
 
 if st.button("시 작성"):
     with st.spinner("시 작성 중..."):
-        result = chat_model.invoke(f"{subject}에 대한 시를 써줘")
+        result = chat_model.invoke(f"{subject}에 대한 시를 윤동주가 쓴 것처럼 써줘")
         st.write(result.content)
 
 
